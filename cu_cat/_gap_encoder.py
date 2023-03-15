e37e6e94ef9110e8b563d81fcf4cb2b48e409742
dc <apple@dc-m1.local>
2023-03-15 15:23:16 +0900

    dont handle unqH,V

--- cu_cat/_gap_encoder.py
+++ cu_cat/_gap_encoder.py
@@ -312,9 +312,9 @@ class GapEncoderColumn(BaseEstimator, TransformerMixin):
             else:
                 if (unq_V.shape[0]*unq_V.shape[1])>2e9 and self.engine=='gpu':
                     if 'cudf.core.dataframe' not in str(getmodule(X)):
-                        self.W_=cp.array(self.W_);unq_V=cp.array(unq_V);unq_H=cp.array(unq_H);self.B_=cp.array(self.B_);self.A_=cp.array(self.A_)
+                        self.W_=cp.array(self.W_);self.B_=cp.array(self.B_);self.A_=cp.array(self.A_); # unq_V=cp.array(unq_V);unq_H=cp.array(unq_H);
                     elif 'cudf.core.dataframe' in str(getmodule(X)):
-                        self.W_=self.W_.to_cupy();unq_V=unq_V.to_cupy();unq_H=unq_H.to_cupy();self.B_=self.B_.to_cupy();self.A_=self.A_.to_cupy()
+                        self.W_=self.W_.to_cupy();self.B_=self.B_.to_cupy();self.A_=self.A_.to_cupy(); # unq_V=unq_V.to_cupy();unq_H=unq_H.to_cupy();
                     logger.debug(f"sent to cupy")
                     # Loop over batches
                 else: #if self.engine!='gpu': #### if 1e9 > samples * features > 2e9 go to cpu even if user wants gpu -- could lessen gap in future
