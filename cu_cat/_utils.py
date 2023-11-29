db5fc0aa46bfb69386548d5c5c682f4526397adc
Daniel <dcolinmorgan@gmail.com>
2023-11-29 15:57:18 +0800

    add cpu mem threshold

--- cu_cat/_utils.py
+++ cu_cat/_utils.py
@@ -99,3 +99,20 @@ def get_gpu_memory():
     memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
     return memory_free_values
 
+def get_sys_memory():
+    """
+    Get node total memory and memory usage
+    """
+    with open('/proc/meminfo', 'r') as mem:
+        ret = {}
+        tmp = 0
+        for i in mem:
+            sline = i.split()
+            if str(sline[0]) == 'MemTotal:':
+                ret['total'] = int(sline[1])
+            elif str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                tmp += int(sline[1])
+        ret['free'] = tmp
+        ret['used'] = int(ret['total']) - int(ret['free'])
+    return ret['free']
+
